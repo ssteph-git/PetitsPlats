22fba33bc7ddb6bc22f85762779b0f459b0c3c26
SIARRAS Stéphane <siarras.stephane@gmail.com>
2022-09-05 01:41:32 -0400

    appareil selector: ok

--- search.js
+++ search.js
@@ -1,9 +1,12 @@
 
 let valeurTapéGlobal=null;
 let valeurTapéIngredient="";
+let valeurTapéAppareil="";
+let valeurTapéUstensile="";
 let tabIngredientTrié=[];
 let tagIngredient=[];
 let ingredientFilter;
+let appareilFilter;
 let messageAucuneRecette = false;
 let tableauRecette = recipes;
 
@@ -21,7 +24,7 @@ function search(){
         resultatRequette = tableauRecette;
     }
 
-
+console.log(resultatRequette);
             /*let triIngredient =  tableauRecette.filter(tab => isNaN( tab.ingredients.filter(tab => tab.ingredient.indexOf(valeurTapéGlobal) != -1)));
             let triIngredient =  tableauRecette.filter(tab => tab.ingredients.filter(tab => tab.ingredient.indexOf(valeurTapéGlobal) != -1));
             let triDescription = tableauRecette.filter((tab => (tab.description.indexOf(valeurTapéGlobal) != -1)));*/
@@ -45,10 +48,26 @@ function search(){
                 })
             }
 
+            /*const removeAppareil = document.querySelectorAll('.optionAppareil');
+            if (removeAppareil != null){ 
+                removeAppareil.forEach((monAppareil) =>{
+                
+                    monAppareil.remove();
+                    
+                })
+            }*/
 
-            const selector = document.getElementById('selector');
+
+            // const selector = document.getElementById('selector');
             const menu = document.getElementById('menuIngredients');
             const tagIngredient = document.querySelector('.tagIngredient');
+
+            // menuAppareil,tagAppareil,appareilExisteDeja);
+            const menuAppareil = document.getElementById('menuAppareils');
+            const tagAppareil = document.querySelector('.tagAppareil');
+
+            const menuUstensile = document.getElementById('menuUstensiles');
+            const tagUsentile = document.querySelector('.tagUstensile');
 
             if(valeurTapéIngredient!=null)
             {
@@ -65,6 +84,23 @@ function search(){
                         });
                     }
             }
+
+            if(valeurTapéAppareil!=null)
+            {
+
+                const allTag = document.getElementsByClassName('pAppareil');
+                if(allTag.length == 0){
+                    appareilFilter =  resultatRequette.filter(tab => tab.appliance.toLowerCase().indexOf(valeurTapéAppareil.toLocaleLowerCase()) != -1);
+                    resultatRequette = appareilFilter;
+                }else{
+                    //Permet de filtrer les résultats du sous menu: en fonction des tag affiché
+                        Array.from(allTag).forEach((allTag, index) => {
+                            appareilFilter =  resultatRequette.filter(tab => tab.appliance.toLowerCase().indexOf(allTag.innerText.toLowerCase()) != -1);
+                            resultatRequette = appareilFilter;
+                        });
+                    }
+            }
+
 // console.log(resultatRequette);
     if(resultatRequette!="")
     {
@@ -77,7 +113,10 @@ function search(){
                 article.setAttribute('class', 'articleRecette');
                 recettes.appendChild(article);
 
-                let { name, description, ingredients, time } = maRecette;
+                let { name, description, ingredients, time, appliance, ustensils } = maRecette;
+
+                // console.log(appliance);
+                // console.log(ustensils);
 
                 //Card Recettes------------------------------------------
                 const imgRecette = document.createElement('img');
@@ -119,38 +158,14 @@ function search(){
                 divMesIngredients.setAttribute('class','mesIngredients');
                 divDescription.appendChild(divMesIngredients);
 
+                let appareilExisteDeja=false;
+                affichageAppareils(appliance,menuAppareil,tagAppareil,appareilExisteDeja);
+
                  //Affichage menu Ingrédients---------------------------------------
                 ingredientExisteDeja=false;
                 ingredients.forEach((ingredient) =>{
                 //Recherche si l'on a tapé quelquche à prendre en compte dans le sous menu des ingredients
                 affichageIngredient(ingredient,menu,tagIngredient,ingredientExisteDeja);
-
-                /*if(ingredient.ingredient.toLowerCase() != valeurTapéIngredient.toLowerCase())
-                // if(ingredient.ingredient.toLowerCase().indexOf(valeurTapéIngredient.toLowerCase()) != -1)
-                {
-                    const option = document.getElementsByClassName('option');
-    
-                    if(option.length != 0){
-                        //Recherche de l'ingrédient dans le DOM
-                        Array.from(option).forEach((element, index) => {
-                            if(element.innerText.toLowerCase() == ingredient.ingredient.toLowerCase()){
-                                ingredientExisteDeja = true;
-                                // console.log("inner: "+element.innerText+" ingr: "+ingredient.ingredient);
-                            }
-                          });
-                          //S'il n'existe pas dans le DOM: on le met dedans
-                        if(ingredientExisteDeja==false){
-                            ajoutIngredientMenu(ingredient,menu,tagIngredient);
-
-                        }else{
-                            ingredientExisteDeja = false;
-                        }
-    
-                    }
-                    else{
-                        ajoutIngredientMenu(ingredient,menu,tagIngredient);
-                    }
-                }*/
                 //Affichage menu Ingrédients---------------------------------------   
 
                 
@@ -256,6 +271,54 @@ function ajoutIngredientMenu(ingredient,menu,tagIngredient){
                                   })
 }
 
+function ajoutAppareilMenu(appareil,menuAppareil,tagAppareil){
+    // console.log(menu);
+     //Pour le premier affichage du "li" (car il n'existe pas en premier)
+    const li = document.createElement('li');
+                                li.setAttribute('class', 'option');
+                                li.textContent = appareil;
+                                menuAppareil.appendChild(li);
+
+                                //Selection, affichage et suppression du TAG
+                                li.addEventListener('click', () => {
+                                    tagAppareil.style.display = 'flex';
+                                    const pAppareil = document.createElement('p');
+                                    pAppareil.setAttribute('class', 'pAppareil');
+                                    pAppareil.textContent = appareil;
+                                    tagAppareil.appendChild(pAppareil);
+
+                                    const iTagAppareil = document.createElement('i');
+                                    iTagAppareil.setAttribute('class', 'fa-regular fa-circle-xmark');
+                                    pAppareil.appendChild(iTagAppareil);
+
+                                    valeurTapéAppareil = appareil;
+
+                                    search();
+
+                                    pAppareil.addEventListener('click', () => { 
+
+                                            /*valeurTapéIngredient = "";
+                                            valeurTapéIngredient = ingredient.ingredient;*/
+                                            pAppareil.remove();//Suppression du TAG du DOM
+                                            // search();
+
+                                        //Recherche des tag dans le dom, à garder après suppression d'un tag
+                                        const allTag = document.getElementsByClassName('pAppareil');
+                                        if(allTag.length != 0){
+                                            Array.from(allTag).forEach((allTag, index) => {
+                                                valeurTapéAppareil = allTag.innerText;
+                                                search();
+                                            });
+                                        }
+                                        else{
+                                            valeurTapéAppareil = "";
+                                            search();
+                                        }
+                                    });
+
+                                  })
+}
+
 function affichageIngredient(ingredient,menu,tagIngredient,ingredientExisteDeja){
     // console.log(ingredient);
     //Recherche si l'on a tapé quelquche à prendre en compte dans le sous menu des ingredients
@@ -287,6 +350,38 @@ function affichageIngredient(ingredient,menu,tagIngredient,ingredientExisteDeja)
 
     }
 }
+// affichageAppareils(appliance,menuAppareil,tagAppareil,appareilExisteDeja);
+function affichageAppareils(appareil,menuAppareil,tagAppareil,appareilExisteDeja){
+    // console.log(ingredient);
+    //Recherche si l'on a tapé quelquche à prendre en compte dans le sous menu des ingredients
+    if(appareil.toLowerCase() != valeurTapéAppareil.toLowerCase())
+    // if(ingredient.ingredient.toLowerCase().indexOf(valeurTapéIngretagIngredientdient.toLowerCase()) != -1)
+    {
+        const option = document.getElementsByClassName('option');
+
+        if(option.length != 0){
+            //Recherche de l'ingrédient dans le DOM
+            Array.from(option).forEach((element, index) => {
+                if(element.innerText.toLowerCase() == appareil.toLowerCase()){
+                    appareilExisteDeja = true;
+                    // console.log("inner: "+element.innerText+" ingr: "+ingredient.ingredient);
+                }
+              });
+              //S'il n'existe pas dans le DOM: on le met dedans
+            if(appareilExisteDeja==false){
+                ajoutAppareilMenu(appareil,menuAppareil,tagAppareil);
+
+            }else{
+                appareilExisteDeja = false;
+            }
+
+        }
+        else{
+            ajoutAppareilMenu(appareil,menuAppareil,tagAppareil);
+        }
+
+    }
+}
 
 
 const formPrimaire = document.getElementById('formPrimaire');
@@ -304,6 +399,7 @@ rechercheIngredient.addEventListener('keydown', () => {
         valeurTapéIngredient = formIngredient.IngredientSearch.value;
 
         const MesIngredients = document.querySelectorAll('.pTitreIngredient');
+        // console.log(MesIngredients);
         let ingredients = [];
 
         MesIngredients.forEach((monIngredient) =>{
@@ -313,7 +409,7 @@ rechercheIngredient.addEventListener('keydown', () => {
         let ingredientFiltré = ingredients.filter((ingredient => (ingredient.ingredient.toLowerCase().indexOf(valeurTapéIngredient.toLowerCase()) != -1)));
         // console.log(ingredientFiltré);
 
-        const removeIngredient = document.querySelectorAll('.option');
+        const removeIngredient = document.querySelectorAll('#menuIngredients>.option');
         if (removeIngredient != null){ 
             removeIngredient.forEach((monIngredient) =>{
             
@@ -327,7 +423,49 @@ rechercheIngredient.addEventListener('keydown', () => {
        
 
         ingredientFiltré.forEach((monFiltre) =>{
-            affichageIngredient(monFiltre,menu,tagIngredient,ingredientExisteDeja);
+            // affichageIngredient(monFiltre,menu,tagIngredient,ingredientExisteDeja);
+            affichageIngredient(monFiltre,menu,tagIngredient,false);
+            
+        
+        // console.log(monFiltre);
+        });
+
+});
+
+const formAppareil = document.getElementById('formAppareil');
+const rechercheAppareil = document.querySelector('.recherche-appareil');
+rechercheAppareil.addEventListener('keydown', () => { 
+        valeurTapéAppareil = formAppareil.AppareilSearch.value;
+
+        const MesAppareils = document.querySelectorAll('#menuAppareils>.option');
+        // console.log(MesAppareils);
+        let appareils = [];
+
+        MesAppareils.forEach((monAppareil) =>{
+            // appareil[appareil.length] = {"ingredient": monIngredient.getAttribute('data-ingredient')};
+            // appareils[appareils.length] = {"ingredient": monAppareil.innerText};
+            appareils[appareils.length] = monAppareil.innerText;
+        });
+        console.log(appareils);
+
+        let appareilFiltré = appareils.filter((appareil => (appareil.toLowerCase().indexOf(valeurTapéAppareil.toLowerCase()) != -1)));
+        console.log(appareilFiltré);
+
+        const removeAppareil = document.querySelectorAll('#menuAppareils>.option');
+        if (removeAppareil != null){ 
+            removeAppareil.forEach((monAppareil) =>{
+            
+                monAppareil.remove();
+                
+            })
+        }
+
+        const menu = document.getElementById('menuAppareils');
+        const tagAppareil = document.querySelector('.tagAppareil');
+       
+
+        appareilFiltré.forEach((monFiltre) =>{
+            affichageAppareils(monFiltre,menu,tagAppareil,false);
             
         
         // console.log(monFiltre);
@@ -340,6 +478,7 @@ const menuIngredientOuvert = document.querySelector('.ouvert');
 const menuIngredientSelector = document.querySelector('#selector');
 menuIngredientFermer.addEventListener('click', () => { 
     menuIngredientFermer.style.display = 'none';
+    // menuIngredientOuvert.style.display = 'flex';
     menuIngredientOuvert.style.display = 'block';
     menuIngredientSelector.style.width = '54%';
 });
@@ -349,6 +488,23 @@ menuIngredientOuvert.addEventListener('click', () => {
     menuIngredientFermer.style.display = 'flex';
 
     menuIngredientSelector.style.width = '13.7%';
+});
+
+const menuAppareilFermer = document.querySelector('.fermerAppareil');
+const menuAppareilOuvert = document.querySelector('.ouvertAppareil');
+const menuAppareilSelector = document.querySelector('#selectorAppareil');
+menuAppareilFermer.addEventListener('click', () => { 
+    menuAppareilFermer.style.display = 'none';
+    // menuAppareilOuvert.style.display = 'flex';
+    menuAppareilOuvert.style.display = 'block';
+    menuAppareilSelector.style.width = '54%';
+});
+
+menuAppareilOuvert.addEventListener('click', () => { 
+    menuAppareilOuvert.style.display = 'none';
+    menuAppareilFermer.style.display = 'flex';
+
+    menuAppareilSelector.style.width = '13.7%';
 });
 
 
